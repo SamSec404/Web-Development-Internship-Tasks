985d32cb70294ab3060a54191673eb42f2d3a13e
Sam <zerodaybypass48@gmail.com>
2026-08-22 21:53:48 +0500

    Task-7 (Problem Solutions) JavaScript Solutions

---
+++ Task-7/Problems/problem-5.js
@@ -0,0 +1,8 @@
+let weight=Number(prompt("Enter your weight in kg:"));
+let height=Number(prompt("Enter your height in meters:"));
+
+let bmi=weight/(height*height);
+
+alert("Your BMI is: " + bmi);
+
+
